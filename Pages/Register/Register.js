b1a14f9afa46b23db5530f1513a69c16958b8563
Troll0128 <pablokiller12.13@gmail.com>
2020-05-27 14:02:07 -0500

    Hola
    Co-Authored-By: null <Troll0128@users.noreply.github.com>

---
+++ Pages/Register/Register.js
@@ -0,0 +1,7 @@
+function Mostrar() {
+  document.getElementById("formulario1").style.display = "block";
+}
+
+if (formulario1.display == "none") {
+  Mostrar();
+}
---
+++ app.js
@@ -0,0 +1,5 @@
+var nombre = prompt("What's your name?");
+// Cajas de dialogo - Metodo Alert()
+
+alert("Welcome to my webpage " + nombre + ", Enjoy!!!");
+document.write("Hello " + nombre + ";");
